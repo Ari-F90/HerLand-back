019a3cb91a2f3cd14955f3ffb26ca723771b3696
Ariadna Frias <ariadna.812@gmail.com>
2023-05-04 15:11:16 +0200

    Update controller test methods

--- src/controllers/figures.controller.test.ts
+++ src/controllers/figures.controller.test.ts
@@ -22,6 +22,51 @@ describe('Given the Figures Controller', () => {
 
   const mockController = new FiguresController(mockRepo);
 
+  describe('When the getAll method is used', () => {
+    (mockRepo.queryAll as jest.Mock).mockResolvedValue([
+      {
+        id: '1',
+        name: 'figure1',
+      },
+      {
+        id: '2',
+        name: 'figure2',
+      },
+    ]);
+    (mockRepo.search as jest.Mock).mockResolvedValue([
+      { id: '1', name: 'figure1', category: 'Science' },
+      { id: '2', name: 'figure2' },
+    ]);
+    test('Then if all the information is OK', async () => {
+      const req = {
+        query: { page: '1' },
+      } as unknown as Request;
+      await mockController.getAll(req, resp, next);
+      expect(mockRepo.queryAll).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then if there are errors, next function should have been called', async () => {
+      const req = {
+        query: { page: '21' },
+      } as unknown as Request;
+      await mockController.getAll(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if there are errors, next function should have been called', async () => {
+      const req = {
+        query: { page: undefined },
+      } as unknown as Request;
+      await mockController.getAll(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if there is a category, the search method should have been called', async () => {
+      const req = {
+        query: { page: '1', category: 'Science' },
+      } as unknown as Request;
+      await mockController.getAll(req, resp, next);
+      expect(mockRepo.search).toHaveBeenCalled();
+    });
+  });
   describe('When the getOneFigure method is used', () => {
     const req = {
       body: {},
@@ -58,6 +103,55 @@ describe('Given the Figures Controller', () => {
     test('Then if there are errors, the next function should have been called', async () => {
       (mockRepo.create as jest.Mock).mockRejectedValue(new Error(''));
       await mockController.post(mockReq, mockResp, mockNext);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When the patch method is used', () => {
+    const req = {
+      body: {
+        id: '1',
+      },
+      params: {
+        id: '1',
+      },
+    } as unknown as Request;
+    const resp = { json: jest.fn() } as unknown as Response;
+    test('Then if all the information is OK, it should update the figure', async () => {
+      await mockController.patch(req, resp, next);
+      expect(mockRepo.update).toHaveBeenCalledWith(req.body);
+      expect(req.params.id).toBe('1');
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then if there are errors, the next function should have been called', async () => {
+      (mockRepo.update as jest.Mock).mockRejectedValue(new Error(''));
+      await mockController.patch(req, resp, next);
+      expect(mockRepo.update).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if there is an error, the next function should have been called', async () => {
+      const req = {
+        body: {
+          id: '2',
+        },
+      } as unknown as Request;
+      (mockRepo.update as jest.Mock).mockRejectedValue(new Error(''));
+      await mockController.patch(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When the delete method is used', () => {
+    const req = {
+      body: {},
+      params: { id: '1' },
+    } as unknown as Request;
+    test('Then if all the information is OK, it should delete the figure', async () => {
+      await mockController.delete(req, resp, next);
+      expect(mockRepo.delete).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then if there are errors, the next function should have been called', async () => {
+      (mockRepo.delete as jest.Mock).mockRejectedValue(new Error(''));
+      await mockController.delete(req, resp, next);
       expect(next).toHaveBeenCalled();
     });
   });
