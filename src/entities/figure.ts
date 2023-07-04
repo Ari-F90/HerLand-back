a5a65f6a9a20ac882bfae273916f8637c672015b
Ariadna Frias <ariadna.812@gmail.com>
2023-07-04 18:18:29 +0200

    Changing props in figure model

--- src/entities/figure.ts
+++ src/entities/figure.ts
@@ -5,5 +5,8 @@ export type Figure = {
   category: string;
   quote: string;
   birth: string;
-  description: string;
+  description1: string;
+  description2: string;
+  description3: string;
+  description4: string;
 };
